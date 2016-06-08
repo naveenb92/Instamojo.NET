a04e50a69f870609ad2528b7b5a148e0fec286e3
Naveen Babu <naveenb@outlook.com>
2016-06-08 17:57:26 +0530

    -> Added the new Build -> Removed the old models.cs file

--- src/Instamojo.NET/Instamojo.cs
+++ src/Instamojo.NET/Instamojo.cs
@@ -1,111 +1,269 @@
-﻿using Instamojo.NET.Models;
+﻿using Instamojo.NET.Exceptions;
+using Instamojo.NET.Models;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Instamojo.NET
 {
     public class Instamojo
     {
-        private WebHeaderCollection Headers;
+        private string api_key;
+        private string auth_token;
 
-        // End Points
-        public String baseURL = "https://www.instamojo.com/api/1.1/";
-        public String EndPoint_PaymentRequest = "payment-requests/";
+        private HttpClient httpClient;
 
-        // Instantiate Instamojo with the API Key and Auth Token
-        public Instamojo(String api_key, String auth_token)
+        /// <summary>
+        // Instantiate Instamojo with the API Key and Auth Token.
+        /// </summary>
+        public Instamojo(string api_key, string auth_token)
         {
-            Headers = new WebHeaderCollection();
-            Headers.Add("X-API-KEY: " + api_key);
-            Headers.Add("X-AUTH-TOKEN: " + auth_token);
+            this.api_key = api_key;
+            this.auth_token = auth_token;
         }
 
-        public PaymentRequestResponse CreatePaymentRequest(PaymentRequest paymentRequest)
+        /// <summary>
+        ///  Create a payment request.
+        /// </summary>
+        /// <returns>An object of type PaymentRequest.</returns>
+        /// <exception cref = "ArgumentNullException"> paymentRequest was null; amount and/or purpose was null; </exception>
+        /// <exception cref = "BadRequestException"> Can happen due to a number of reasons. </exception>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        public async Task<PaymentRequest> CreatePaymentRequest(PaymentRequest paymentRequest)
         {
-            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(baseURL + EndPoint_PaymentRequest);
-            httpReq.Headers = Headers;
-            httpReq.ContentType = "application/json";
-            httpReq.Method = "POST";
-            String json = JsonConvert.SerializeObject(paymentRequest, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            using (var writer = new StreamWriter(httpReq.GetRequestStream()))
+            if (paymentRequest == null)
+                throw new ArgumentNullException("paymentRequest cannot be null");
+
+            if (paymentRequest.amount == null)
+                throw new ArgumentNullException("paymentRequest.amount");
+
+            if (paymentRequest.purpose == null)
+                throw new ArgumentNullException("paymentRequest.purpose");
+
+            StringContent jsonContent = new StringContent(JsonConvert.SerializeObject(paymentRequest, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }), Encoding.UTF8, "application/json");
+
+            using (httpClient = new HttpClient())
             {
-                writer.Write(json);
-                writer.Flush();
-                writer.Close();
-            }
-            using (HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse())
-            {
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.PostAsync(Endpoints.paymentRequests, jsonContent);
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.Created)
                 {
-                    PaymentRequestResponse prr = JsonConvert.DeserializeObject<PaymentRequestResponse>(reader.ReadToEnd());
-                    reader.Close();
-                    response.Close();
-                    return prr;
+                    return JsonConvert.DeserializeObject<PaymentRequestResponse>(content).payment_request;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+                else
+                {
+                    Dictionary<string, dynamic> values = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(content);
+                    throw new BadRequestException(values["message"].ToString());
                 }
             }
         }
 
-        public PaymentRequestResponse GetPaymentRequestStatus(String id)
+        /// <summary>
+        ///  Get all payment request.
+        /// </summary>
+        /// <returns>A List of PaymentRequest objects.</returns>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        public async Task<List<PaymentRequest>> GetPaymentRequests()
         {
-            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(baseURL + EndPoint_PaymentRequest + id + "/");
-            httpReq.Headers = Headers;
-            httpReq.Method = "GET";
-            using (HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse())
+            using (httpClient = new HttpClient())
             {
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.GetAsync(Endpoints.paymentRequests);
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
                 {
-                    PaymentRequestResponse jobj = JsonConvert.DeserializeObject<PaymentRequestResponse>(reader.ReadToEnd());
-                    reader.Close();
-                    response.Close();
-                    return jobj;
+                    return JsonConvert.DeserializeObject<PaymentRequestsResponse>(content).payment_requests;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        ///  Get a payment request.
+        /// </summary>
+        /// <returns>A PaymentRequest object.</returns>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        /// <exception cref = "PaymentRequestNotFoundException"> Raised if Payment request is not found. </exception>
+        public async Task<PaymentRequest> GetPaymentRequest(string id)
+        {
+            using (httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.GetAsync(Endpoints.paymentRequest(id));
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return JsonConvert.DeserializeObject<PaymentRequestResponse>(content).payment_request;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+                else
+                {
+                    throw new PaymentRequestNotFoundException(id);
                 }
             }
         }
 
-        public PaymentRequestsResponse ListPaymentRequests()
+        /// <summary>
+        ///  Get a payment.
+        /// </summary>
+        /// <returns>A Payment object.</returns>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        /// <exception cref = "PaymentNotFoundException"> Raised if Payment is not found. </exception>
+        public async Task<Payment> GetPayment(string id, string payment_id)
         {
-            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(baseURL + EndPoint_PaymentRequest);
-            httpReq.Headers = Headers;
-            httpReq.Method = "GET";
-            using (HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse())
+            using (httpClient = new HttpClient())
             {
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.GetAsync(Endpoints.payment(id, payment_id));
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
                 {
-                    PaymentRequestsResponse prr = JsonConvert.DeserializeObject<PaymentRequestsResponse>(reader.ReadToEnd());
-                    reader.Close();
-                    response.Close();
-                    return prr;
+                    return JsonConvert.DeserializeObject<PaymentRequestResponseForPayment>(content).payment_request.payment;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+                else
+                {
+                    throw new PaymentNotFoundException(id, payment_id);
                 }
             }
         }
 
-        public PaymentRequestsResponse ListPaymentRequests(DateTime? min_created_at = null, DateTime? max_created_at = null, DateTime? min_modified_at = null, DateTime? max_modified_at = null)
+        /// <summary>
+        ///  Create a refund.
+        /// </summary>
+        /// <returns>An object of type Refund.</returns>
+        /// <exception cref = "ArgumentNullException"> refund was null; payment_id was null; </exception>
+        /// <exception cref = "BadRequestException"> Can happen due to a number of reasons. </exception>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        public async Task<Refund> CreateRefund(Refund refund)
         {
-            String temp = "";
-            if (min_created_at != null)
-                temp += (temp == "") ? "?min_created_at=" + min_created_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture) : "&min_created_at=" + min_created_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
-            if (max_created_at != null)
-                temp += (temp == "") ? "?max_created_at=" + max_created_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture) : "&max_created_at=" + max_created_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
-            if (min_modified_at != null)
-                temp += (temp == "") ? "?min_modified_at=" + min_modified_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture) : "&min_modified_at=" + min_modified_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
-            if (max_modified_at != null)
-                temp += (temp == "") ? "?max_modified_at=" + max_modified_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture) : "&max_modified_at=" + max_modified_at.Value.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+            if (refund == null)
+                throw new ArgumentNullException("refund");
 
-            String FilteredURL = baseURL + EndPoint_PaymentRequest + temp;
-            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(FilteredURL);
-            httpReq.Headers = Headers;
-            httpReq.Method = "GET";
+            if (refund.payment_id == null)
+                throw new ArgumentNullException("refund.payment_id");
 
-            using (HttpWebResponse response = (HttpWebResponse)httpReq.GetResponse())
+            StringContent jsonContent = new StringContent(JsonConvert.SerializeObject(refund, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }), Encoding.UTF8, "application/json");
+
+            using (httpClient = new HttpClient())
             {
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.PostAsync(Endpoints.refunds, jsonContent);
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.Created)
                 {
-                    PaymentRequestsResponse jobj = JsonConvert.DeserializeObject<PaymentRequestsResponse>(reader.ReadToEnd());
-                    reader.Close();
-                    response.Close();
-                    return jobj;
+                    return JsonConvert.DeserializeObject<RefundResponse>(content).refund;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+                else
+                {
+                    Dictionary<string, dynamic> values = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(content);
+                    throw new BadRequestException(values["message"].ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Get all refunds.
+        /// </summary>
+        /// <returns>A List of Refund objects.</returns>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        public async Task<List<Refund>> GetRefunds()
+        {
+            using (httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.GetAsync(Endpoints.refunds);
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return JsonConvert.DeserializeObject<RefundsResponse>(content).refunds;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+
+                return null;
+            }
+        }
+
+        /// <summary>
+        ///  Get a refund.
+        /// </summary>
+        /// <returns>A Refund object.</returns>
+        /// <exception cref = "UnauthorizedAccessException"> Raised due to invalid credentials. </exception>
+        /// <exception cref = "RefundNotFoundException"> Raised if refund is not found. </exception>
+        public async Task<Refund> GetRefund(string id)
+        {
+            using (httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(Endpoints.baseURL);
+                httpClient.DefaultRequestHeaders.Add("X-API-KEY", this.api_key);
+                httpClient.DefaultRequestHeaders.Add("X-AUTH-TOKEN", this.auth_token);
+
+                var response = await httpClient.GetAsync(Endpoints.refund(id));
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return JsonConvert.DeserializeObject<RefundResponse>(content).refund;
+                }
+                else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    throw new UnauthorizedAccessException("Please check the apiKey and authToken.");
+                }
+                else
+                {
+                    throw new RefundNotFoundException(id);
                 }
             }
         }
